df00d065129a49538202e5fe8e2e5d609841ef23
Andy <alipinski@mail.com>
2022-10-19 10:40:25 +0200

    styling + passing props to Sub for dynamic display

--- src/components/subscription/Sub.js
+++ src/components/subscription/Sub.js
@@ -1,9 +1,24 @@
 import './Sub.css';
 
-const Sub = () => {
+const Sub = ({ plan, theme, price }) => {
   return (
-    <div>
-      <h1>Sub</h1>
+    <div className='price-box --card'>
+      <div className={`box --p2 ${theme}`}>
+        <p className='--text-light'>{plan}</p>
+        <h4 className='--text-light'>
+          <span>$</span>
+          <span className='basic'>{price}</span>
+        </h4>
+      </div>
+      <div className='features'>
+        <ul>
+          <li>Unlimited Pages</li>
+          <li>Unlimited Bandwith</li>
+
+          <li>500GB Storage</li>
+        </ul>
+        <button className={`btn ${theme}`}>Buy Now</button>
+      </div>
     </div>
   );
 };
--- src/components/subscription/SubList.js
+++ src/components/subscription/SubList.js
@@ -3,9 +3,30 @@ import Sub from './Sub';
 
 const SubList = () => {
   return (
-    <div>
-      <Sub />
-    </div>
+    <section className='main'>
+      <div className='container --center-all '>
+        <div className='title'>
+          <h2>Pricing</h2>
+          <div className='--line'></div>
+
+          <div className='--flex-center --my2'>
+            <p>Monthly</p>
+            <div className='--mx2'>
+              <span className='toggle-btn'>
+                <div className='ball'></div>
+              </span>
+            </div>
+            <p>Yearly</p>
+          </div>
+        </div>
+
+        <div className='sub-plans'>
+          <Sub plan={'Basic'} theme={'theme1'} price={'10'} />
+          <Sub plan={'Pro'} theme={'theme2'} price={'20'} />
+          <Sub plan={'Master'} theme={'theme3'} price={'30'} />
+        </div>
+      </div>
+    </section>
   );
 };
 
